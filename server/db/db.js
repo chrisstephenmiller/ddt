69fb3a04cad832d2ced728b19355f8ce619728e6
cmiller <christopher.miller@here.com>
2019-08-29 14:55:16 -0500

    first deploy

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name
-const db = new Sequelize(process.env.DATABASE_NAME || databaseName, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
+const db = new Sequelize(process.env.DATABASE_URL || databaseName, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
   host: process.env.DATABASE_HOST || 'localhost',
   port: 5432,
   dialect: 'postgres',
