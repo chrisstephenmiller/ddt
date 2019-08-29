01802afa37bc272de39dbb10bd97d0d1d84239b1
cmiller <christopher.miller@here.com>
2019-08-29 16:41:38 -0500

    first deploy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const db = require('./db')
 const path = require('path')
 
